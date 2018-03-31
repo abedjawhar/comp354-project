e9c0eaf894a8867e7198185a2565cba4cf0c0fbf
Marc-Antoine Dube <madube94@gmail.com>
2018-03-31 14:58:20 -0400

    [FEAT] Complete test table script generator templating

--- scripts/test-table-generator.js
+++ scripts/test-table-generator.js
@@ -80,10 +80,10 @@ let json = {
     }
 };
 
-const firstColSize = '3cm';
-const dataColSize = '12cm';
+const firstColSize = '4cm';
+const dataColSize = '13cm';
 
-const variableCount = getVariableCount();
+const maxTestVarCount = getMaxVarCount();
 
 let table = generateTableTop() + '\n'
     + generateRow('Tester Name', json.testerName) + '\n'
@@ -91,16 +91,48 @@ let table = generateTableTop() + '\n'
     + generateRow('Class Name', json.className) + '\n'
     + generateRow('Method Name', json.methodName) + '\n'
     + generateRow('Purpose', json.purpose) + '\n'
-    + generateRow('Use Cases', listToItems(json.useCases)) + '\n'
-    + `\\multicolumn{${(1 + variableCount)}}{|l|}{\\cellcolor[HTML]{C0C0C0}\\textbf{Test Scenarios}}\\\\ \\hline`  + '\n';
+    + generateRow('Use Cases', listToTable(json.useCases)) + '\n'
+    + `\\multicolumn{${(1 + maxTestVarCount)}}{|l|}{\\cellcolor[HTML]{C0C0C0}\\textbf{Test Scenarios}}\\\\ \\hline`  + '\n';
 
 Object.keys(json.testScenarios).forEach((scenarioName) => {
-    table += `\\multicolumn{${(1 + variableCount)}}{|l|}{${stripUnderscores(scenarioName)}}\\\\ \\hline` + '\n';
+    table += `\\multicolumn{${(1 + maxTestVarCount)}}{|l|}{${stripUnderscores(scenarioName)}}\\\\ \\hline` + '\n';
+
+    const scenario = json.testScenarios[scenarioName];
+    const inputSpecification = scenario.inputSpecification;
+    const testVarCount = Object.keys(inputSpecification).length;
+    let headerVariableRow = '\\rowcolor[HTML]{C0C0C0} ';
+    let valueRow = '\\multirow{-2}{*}{\\cellcolor[HTML]{C0C0C0}\\textbf{Input Specification}} ';
+    Object.keys(inputSpecification).forEach((varName, index) => {
+        let headerCol = '& ';
+        let valueCol = '&';
+        // IF a test has less variables than the test that has the most variables,
+        // we need to use a multicolumn for the last column to take the whole width
+        if (testVarCount < maxTestVarCount && (index + 1) === testVarCount) {
+            headerCol += ` \\multicolumn{${maxTestVarCount - index}}{l|}{${varName}} `;
+            valueCol += ` \\multicolumn{${maxTestVarCount - index}}{l|}{${formatValue(inputSpecification[varName])}} `;
+        } else {
+            headerCol += ` ${varName} `;
+            valueCol += formatValue(inputSpecification[varName]);
+        }
+        headerVariableRow += headerCol;
+        valueRow += valueCol;
+    });
+    headerVariableRow += '\\\\ \\cline{2-3} ' + '\n';
+    valueRow += '\\\\ \\hline' + '\n';
+
+    table += headerVariableRow + valueRow;
+    table += generateRow('Expected Output', listToTable(scenario.expectedOutput)) + '\n';
+    table += generateRow('Actual Output', listToTable(scenario.actualOutput)) + '\n';
+    table += generateRow('Bug Found', scenario.bugFound) + '\n';
+    table += generateRow('Purpose', scenario.purpose) + '\n';
+
 });
 
-console.log(table)
+table += '\\end{longtable}\n';
 
-function getVariableCount() {
+console.log(table);
+
+function getMaxVarCount() {
     let vars = 0;
     Object.keys(json.testScenarios).forEach((key) => {
         const varCount = Object.keys(json.testScenarios[key].inputSpecification).length;
@@ -108,31 +140,44 @@ function getVariableCount() {
             vars = varCount;
         }
     });
-    return vars
+    return vars;
 }
 
 function generateTableTop() {
     let cols = '';
-    for (let i = 0; i < variableCount; i++) {
+    for (let i = 0; i < maxTestVarCount; i++) {
         cols += 'l|';
     }
-    return `\\begin{table}[]\\centering\\caption{${json.className}.${json.methodName}`
-        + `}\\label{my-label}\\begin{tabular}{|m{${firstColSize}}|l|${cols}}\\hline`;
+    return `\\begin{longtable}{|m{${firstColSize}}|l|${cols}}`
+        + `\\caption[]{${json.className}.${json.methodName}}`
+        + `\\hline`;
 }
 
 function generateRow(label, value) {
-    return `\\cellcolor[HTML]{C0C0C0}\\textbf{${label}} & \\multicolumn{${variableCount}}`
+    return `\\cellcolor[HTML]{C0C0C0}\\textbf{${label}} & \\multicolumn{${maxTestVarCount}}`
         + `{p{${dataColSize}}|}{${value}}\\\\ \\hline`;
 }
 
-function listToItems(list) {
+function listToTable(list) {
     let itemStrings = '';
     list.forEach(function(item) {
-        itemStrings += '\\item ' + item;
+        itemStrings += `${item}\\\\ `;
     });
-    return `\\begin{itemize}${itemStrings}\\end{itemize}`;
+    return `\\begin{tabular}[c]{@{}l@{}}${itemStrings}\\end{tabular}`;
 }
 
 function stripUnderscores(str) {
     return str.replace(/_/g, '\\_');
+}
+
+function formatValue(value) {
+    if (value && value instanceof Object) {
+        const arr = [];
+        Object.keys(value).forEach((v) => {
+            arr.push(`${v}: ${value[v]}`);
+        });
+        return ` ${listToTable(arr)}`;
+    } else {
+        return ` ${value} `;
+    }
 }
