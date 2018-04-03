f4325e4d5d9a5507bda9a0b428d869719db5250e
Kadeem Caines <kadeemcaines@gmail.com>
2018-04-03 14:33:14 -0400

    created method called GenerateFile()
    -created : private TransactionGenerateFileController generator = new TransactionGenerateFileController();
    -created :private List<Account> accounts;
    
    this method is used to generate a CSV that will list all the transactions for every account. It creates a
    an array list of Transactions, then goes through a for loop that will get every transaction related to each account
    in the List<Account> accounts arraylist. It then passes the transactions List through the GenerateFile method paramater, then names the file, then finally generates a CSV

--- src/main/java/com/github/comp354project/viewController/AllTransactionsController.java
+++ src/main/java/com/github/comp354project/viewController/AllTransactionsController.java
@@ -3,6 +3,7 @@ package com.github.comp354project.viewController;
 import com.github.comp354project.MyMoneyApplication;
 import com.github.comp354project.model.account.Account;
 import com.github.comp354project.model.account.Transaction;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
@@ -16,6 +17,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
+
+
 public class AllTransactionsController implements Initializable {
 
     @FXML private Label totalBalanceLabel;
@@ -25,10 +28,18 @@ public class AllTransactionsController implements Initializable {
 
     @FXML private TransactionTableController transactionTableViewController;
 
+
+    private TransactionGenerateFileController generator = new TransactionGenerateFileController();
+
+
+    private List<Account> accounts;
+
+
     public void initialize(URL url, ResourceBundle rb) {
     }
 
     public void setAccounts(List<Account> accounts){
+        this.accounts = accounts;
         Double balance = 0.0;
         List<Transaction> transactions = new ArrayList<>();
         for(Account a : accounts){
@@ -39,10 +50,47 @@ public class AllTransactionsController implements Initializable {
         this.totalBalanceLabel.setText("$" + balance);
         this.descriptionLabel.setText("All Transactions");
         AnchorPane.setTopAnchor(descriptionLabel, 15.0);
+
     }
 
     @FXML
     public void gotoAccountList(MouseEvent event) throws IOException {
         MyMoneyApplication.application.displayAccounts();
     }
-}
+
+
+    /**
+     * this method is used to generate a CSV that will list all the transactions for every account. It creates a
+     * an array list of Transactions, then goes through a for loop that will get every transaction related to each account
+     * in the List<Account> accounts arraylist. It then passes the transactions List through the GenerateFile method paramater, then names the file,
+     * then finally generates a CSV
+     */
+    @FXML
+    public void generateAllTransactions() {
+        List<Transaction> transactions = new ArrayList<>();
+        if(accounts != null) {
+            for(Account a : accounts){
+                transactions.addAll(a.getTransactions());
+            }
+        }
+
+        try {
+            generator.GenerateFile(transactions,"AllTransactions.csv");
+            System.out.println("File" +" "+"AllTransactions.csv"+" "+"created");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    }
+
+
+
+
+
+
+
+
+
